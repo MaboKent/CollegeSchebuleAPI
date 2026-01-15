e4d44563940455c068444356aa5d91f157f4190d
unknown <maksimboarki142033@gmail.com>
2026-01-15 15:57:01 +0700

    Регистрация сервиса в Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using CollegeSchedule.Data;
+using CollegeSchedule.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,7 +18,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IScheduleService, ScheduleService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
