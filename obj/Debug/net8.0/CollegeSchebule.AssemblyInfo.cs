1e6357229411198fe388ffeb6ae72b325099d952
unknown <maksimboarki142033@gmail.com>
2026-01-15 15:53:52 +0700

    Добавлен и заполнен файл ScheduleService.cs

--- obj/Debug/net8.0/CollegeSchebule.AssemblyInfo.cs
+++ obj/Debug/net8.0/CollegeSchebule.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeSchebule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abcd0f55a57e3eec24b2879a8866d9a3291ce3f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68ace7307dae8e5af4bc49e437fd3bf714c53120")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeSchebule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeSchebule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
